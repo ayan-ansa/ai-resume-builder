834bf9881bf79f1d454a28fe19337afe5be4820a
Ayan Ansari <mayan9917@gmail.com>
2024-12-08 21:20:29 +0530

    changed common js module require to ESM import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -59,5 +60,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
